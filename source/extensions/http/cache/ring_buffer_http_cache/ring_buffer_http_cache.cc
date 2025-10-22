6d4c5092c2158cbdf0bc86a095c492de298edf4b
Michal Dvořák <dvora124@pm.me>
2025-10-22 19:32:10 +0200

    Move implementation details from .h to .cc

--- source/extensions/http/cache/ring_buffer_http_cache/ring_buffer_http_cache.cc
+++ source/extensions/http/cache/ring_buffer_http_cache/ring_buffer_http_cache.cc
@@ -15,9 +15,167 @@ namespace Cache
 namespace
 {
 
-  auto  g_Register  = Registry::RegisterFactory<RingBufferHttpCacheFactory, HttpCacheFactory>  ();
+auto  g_Register  = Registry::RegisterFactory<RingBufferHttpCacheFactory, HttpCacheFactory>  ();
 
 } // namespace
+
+
+///  represents a single lookup operation.
+struct  RingBufferLookupContext : public LookupContext
+{
+  Event::Dispatcher  & m_Dispatcher;
+  LookupRequest  m_Request;
+  //  simple http cache uses std::shared_ptr<bool>, but i dunno why, it doesn't make it thread safe
+  bool  m_Stop = false;
+
+
+  explicit  RingBufferLookupContext (
+    Event::Dispatcher   & dispatcher,
+    LookupRequest      && request
+  )
+    : m_Dispatcher  { dispatcher },
+      m_Request     { std::move ( request ) }
+  {
+  }
+  // ------------------------------------------------------------------------
+  auto  getHeaders ( LookupHeadersCallback  && callback )
+    -> void
+    override
+  {
+    // probe cache for `m_Request . key ()`.
+    // if found:  create `LookupResult` and pass it to the %(callback)
+    // not found
+    m_Dispatcher . post ( [ callback = std::move ( callback ), stop = &m_Stop ] ( ) mutable
+      -> void
+    {
+      if ( !(*stop) ) {
+        std::move ( callback ) ( LookupResult {}, /* ???= */ false );
+      }
+    } );
+  }
+  // ------------------------------------------------------------------------
+  auto  getTrailers ( LookupTrailersCallback  && callback )
+    -> void
+    override
+  {
+    assert ( 0 );
+  }
+  // ------------------------------------------------------------------------
+  auto  getBody (
+    const AdjustedByteRange  & range,
+    LookupBodyCallback      && callback
+  )
+    -> void
+    override
+  {
+    assert ( 0 );
+  }
+  // ------------------------------------------------------------------------
+  auto  onDestroy ( )
+    -> void
+    override
+  {
+    m_Stop = true;
+  }
+};
+
+///  represents a single insert operation.
+struct  RingBufferInsertContext : public InsertContext
+{
+
+
+  auto  insertHeaders (
+    const Http::ResponseHeaderMap  & response_headers,
+    const ResponseMetadata         & metadata,
+    InsertCallback                   insert_complete,
+    bool                             end_stream
+  )
+    -> void
+    override
+  {
+
+    assert ( 0 );
+  }
+  // ------------------------------------------------------------------------
+  auto  insertBody (
+    const Buffer::Instance  & fragment,
+    InsertCallback            ready_for_next_fragment,
+    bool                      end_stream
+  )
+    -> void
+    override
+  {
+    assert ( 0 );
+  }
+  // ------------------------------------------------------------------------
+  auto insertTrailers (
+    const Http::ResponseTrailerMap  & trailers,
+    InsertCallback                    insert_complete
+  )
+    -> void
+    override
+  {
+    assert ( 0 );
+  }
+  // ------------------------------------------------------------------------
+  auto onDestroy ( )
+    -> void
+    override
+  {
+    assert ( 0 );
+  }
+};
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+[[nodiscard]]
+auto  RingBufferHttpCache::makeLookupContext (
+  LookupRequest                 && request,
+  Http::StreamFilterCallbacks    & callbacks
+)
+  -> LookupContextPtr
+{
+  return  std::make_unique<RingBufferLookupContext> ( callbacks . dispatcher (), std::move ( request ) );
+}
+// --------------------------------------------------------------------------
+[[nodiscard]]
+auto  RingBufferHttpCache::makeInsertContext (
+  LookupContextPtr              && lookup_context,
+  Http::StreamFilterCallbacks    & callbacks
+)
+  -> InsertContextPtr
+{
+  return  std::make_unique<RingBufferInsertContext> ();
+}
+// --------------------------------------------------------------------------
+auto  RingBufferHttpCache::updateHeaders (
+  const LookupContext            & lookup_context,
+  const Http::ResponseHeaderMap  & response_headers,
+  const ResponseMetadata         & metadata,
+  UpdateHeadersCallback            on_complete
+)
+  -> void
+{
+  assert ( 0 );
+}
+// --------------------------------------------------------------------------
+[[nodiscard]]
+auto  RingBufferHttpCache::cacheInfo ( ) const
+  -> CacheInfo
+{
+  return  CacheInfo { . name_ = Self::CACHE_INFO_NAME };
+}
+// --------------------------------------------------------------------------
+[[nodiscard]]
+auto  RingBufferHttpCache::lookup ( const LookupRequest  & request ) const
+  -> bool
+{
+  assert ( 0 );
+}
+
+
 
 } // namespace Cache
 } // namespace HttpFilters
--- source/extensions/http/cache/ring_buffer_http_cache/ring_buffer_http_cache.h
+++ source/extensions/http/cache/ring_buffer_http_cache/ring_buffer_http_cache.h
@@ -17,90 +17,6 @@ namespace Cache
 {
 
 
-struct  RingBufferLookupContext : public LookupContext
-{
-
-  // ------------------------------------------------------------------------
-  auto  getHeaders ( LookupHeadersCallback  && callback )
-    -> void
-    override
-  {
-    // probe cache for `m_Request . key ()`.
-    // if found:  create `LookupResult` and pass it to the %(callback)
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
-  auto  getTrailers ( LookupTrailersCallback  && callback )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
-  auto  getBody (
-    const AdjustedByteRange  & range,
-    LookupBodyCallback      && callback
-  )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
-  auto  onDestroy ( )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-};
-
-struct  RingBufferInsertContext : public InsertContext
-{
-
-  // ------------------------------------------------------------------------
-  auto  insertHeaders (
-    const Http::ResponseHeaderMap  & response_headers,
-    const ResponseMetadata         & metadata,
-    InsertCallback                   insert_complete,
-    bool                             end_stream
-  )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
-  auto  insertBody (
-    const Buffer::Instance  & fragment,
-    InsertCallback            ready_for_next_fragment,
-    bool                      end_stream
-  )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
-  auto insertTrailers (
-    const Http::ResponseTrailerMap  & trailers,
-    InsertCallback                    insert_complete
-  )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
-  auto onDestroy ( )
-    -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-};
-
-
 struct  RingBufferHttpCache : public HttpCache
 {
   using  Self = RingBufferHttpCache;
@@ -110,30 +26,22 @@ struct  RingBufferHttpCache : public HttpCache
   // TODO:
   // ring buffer
 
-  // ------------------------------------------------------------------------
-
+  [[nodiscard]]
   auto  makeLookupContext (
     LookupRequest                 && request,
     Http::StreamFilterCallbacks    & callbacks
   )
     -> LookupContextPtr
-    override
-  {
-    return  std::make_unique<RingBufferLookupContext> ();
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
+    override;
+
+  [[nodiscard]]
   auto  makeInsertContext (
     LookupContextPtr              && lookup_context,
     Http::StreamFilterCallbacks    & callbacks
   )
     -> InsertContextPtr
-    override
-  {
-    return  std::make_unique<RingBufferInsertContext> ();
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
+    override;
+
   auto  updateHeaders (
     const LookupContext            & lookup_context,
     const Http::ResponseHeaderMap  & response_headers,
@@ -141,19 +49,18 @@ struct  RingBufferHttpCache : public HttpCache
     UpdateHeadersCallback            on_complete
   )
     -> void
-    override
-  {
-    PANIC ( "not yet implemented!" );
-  }
-  // ------------------------------------------------------------------------
+    override;
+
+  [[nodiscard]]
   auto  cacheInfo ( ) const
     -> CacheInfo
-    override
-  {
-    using namespace std::string_view_literals;
-    return  CacheInfo { . name_ = Self::CACHE_INFO_NAME };
-    PANIC ( "not yet implemented!" );
-  }
+    override;
+
+
+  [[nodiscard]]
+  auto  lookup ( const LookupRequest  & request ) const
+    -> bool;
+
 };
 
 struct  RingBufferHttpCacheFactory : public HttpCacheFactory
@@ -161,23 +68,24 @@ struct  RingBufferHttpCacheFactory : public HttpCacheFactory
   using  Self = RingBufferHttpCacheFactory;
 
   // ------------------------------------------------------------------------
+  [[nodiscard]]
   auto  name ( ) const
     -> std::string
     override
   {
     return  std::string { RingBufferHttpCache::CACHE_INFO_NAME };
-    PANIC ( "not yet implemented!" );
   }
   // ------------------------------------------------------------------------
+  [[nodiscard]]
   auto  createEmptyConfigProto ( )
     -> ProtobufTypes::MessagePtr
     override
   {
     return  std::make_unique<ProtobufWkt::Empty>  ();
     //return  std::make_unique<envoy::extensions::http::cache::simple_http_cache::v3::RingBufferHttpCacheConfig>  ();
-    PANIC ( "not yet implemented!" );
   }
   // ------------------------------------------------------------------------
+  [[nodiscard]]
   auto  getCache (
     [[maybe_unused]]
     const envoy::extensions::filters::http::cache::v3::CacheConfig   & config,  // the fuck?
@@ -187,10 +95,8 @@ struct  RingBufferHttpCacheFactory : public HttpCacheFactory
     override
   {
     return  std::make_shared<RingBufferHttpCache> ();
-    PANIC ( "not yet implemented!" );
   }
 };
-
 
 } // namespace Cache
 } // namespace HttpFilters
