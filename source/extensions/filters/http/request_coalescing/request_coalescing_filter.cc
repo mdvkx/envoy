9878ef98477051b86bf3a10a55a387a8a28409a8
Michal Dvořák <dvora124@pm.me>
2026-03-25 16:04:01 +0100

    sq

--- source/extensions/filters/http/request_coalescing/request_coalescing_filter.cc
+++ source/extensions/filters/http/request_coalescing/request_coalescing_filter.cc
@@ -1,8 +1,12 @@
 //#include "source/extensions/filters/http/request_coalescing/request_coalescing_filter.h"
 #include "./request_coalescing_filter.h"
-#include "envoy/registry/registry.h"  //
-#include "source/common/common/logger.h"  // ENVOY_LOG_xxx
+#include "envoy/http/codes.h"  // Http::Code
+#include "envoy/http/header_map.h"  // HeaderMap
+#include "envoy/registry/registry.h"  // Registry::RegisterFactory
+#include "source/common/common/logger.h"  // ENVOY_LOG_MISC
+#include "source/common/http/header_map_impl.h"  // createHeaderMap
 #include <memory>  // shared ptr
+#include <optional>  // optional
 namespace  Envoy::Extensions::HttpFilters::RequestCoalescing
 {
 
@@ -11,6 +15,43 @@ namespace
   auto  g_SelfRegister = Registry::RegisterFactory<RequestCoalescingFilterFactory, Server::Configuration::NamedHttpFilterConfigFactory> {};
 }
 
+const auto  g_CacheableStatusCodes = std::unordered_set<std::string_view>
+{  // unordered_set doesn't have a constexpr constructor, jeeeeeesus.
+  "200", "203", "204", "206",
+  "300", "301", "308",
+  "404", "405", "410", "414", "451",
+  "501",
+};
+// --------------------------------------------------------------------------
+static auto  is_cacheable ( const Http::ResponseHeaderMap & headers ) -> bool
+{
+  if ( !g_CacheableStatusCodes . contains ( headers . getStatusValue () ) )
+    return  false;
+  //if ( auto  * p = headers . CacheControl (); p == nullptr ||   )
+  return  true;
+}
+// --------------------------------------------------------------------------
+static auto  is_cacheable ( const Response & response ) -> bool
+{
+  return  response . m_Headers != nullptr && is_cacheable ( *response . m_Headers );
+}
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+auto  Cache::lookup ( const std::string & key ) const -> std::optional<const Response *>
+{
+  auto  l = std::unique_lock { m_Mtx };
+  if ( auto  i = m_Cache . find ( key ); i != m_Cache . end () )
+    return  std::addressof ( i -> second );
+  else
+    return  std::nullopt;
+}
+// --------------------------------------------------------------------------
+auto  Cache::insert ( const std::string & key,
+                      Response && value ) -> void
+{
+  auto  l = std::unique_lock { m_Mtx };
+  m_Cache [ key ] = std::move ( value );
+}
 // --------------------------------------------------------------------------
 // --------------------------------------------------------------------------
       RequestCoalescingFilter::RequestCoalescingFilter ( std::shared_ptr<Config>  config,
@@ -20,23 +61,62 @@ namespace
 {
 }
 // --------------------------------------------------------------------------
+auto  RequestCoalescingFilter::commit ( ) -> void
+{
+  if ( !is_cacheable ( m_Response ) )
+    return;
+  m_Cache -> insert ( m_Key, std::move ( m_Response ) );
+  //assert ( 0 );
+}
+// --------------------------------------------------------------------------
 auto  RequestCoalescingFilter::decodeHeaders ( Http::RequestHeaderMap & headers,
                                                bool  is_last ) -> Http::FilterHeadersStatus
 {
-  ENVOY_LOG_MISC ( debug, "RequestCoalescingFilter::decodeHeaders (), headers = {}", headers );
-  // if cacheable
-  auto  scheme  = headers . getSchemeValue ();
-  auto  host    = headers . getHostValue ();
-  auto  path    = headers . getPathValue ();
-  auto  key     = absl::StrCat ( scheme, host, path );
-  return  Http::FilterHeadersStatus::Continue;
+  using namespace  std::literals;
+  const auto  scheme = headers . getSchemeValue ();
+  const auto  host = headers . getHostValue ();
+  const auto  path = headers . getPathValue ();
+  const auto  key = absl::StrCat ( scheme, "://", host, path );
+  m_Key = key;
+  const auto  response = m_Cache -> lookup ( key );
+  if ( !response  // cache miss
+       ||  std::chrono::duration_cast<std::chrono::seconds> ( (m_Config -> m_Time . systemTime () - (*response) -> m_Stamp) ) > 60s )  // expired
+    return  Http::FilterHeadersStatus::Continue;
+  decoder_callbacks_ -> sendLocalReply ( Http::Code::OK, response . value () -> m_Body, [ response ] ( Http::ResponseHeaderMap & headers ) -> void
+  {
+    // TODO: what about original headers?
+    // TODO: response may dangle! it's a reference into a shared cache
+    headers = *response . value () -> m_Headers;
+  }, absl::nullopt, "details"sv );
+  return  Http::FilterHeadersStatus::StopAllIterationAndWatermark;
+  assert ( 0 );
 }
 // --------------------------------------------------------------------------
 auto  RequestCoalescingFilter::encodeHeaders ( Http::ResponseHeaderMap & headers,
                                                bool  is_last ) -> Http::FilterHeadersStatus
 {
-  ENVOY_LOG_MISC ( debug, "RequestCoalescingFilter::encodeHeaders (), headers = {}", headers );
+  m_Response . m_Headers = Http::createHeaderMap<Http::ResponseHeaderMapImpl> ( headers );
+  m_Response . m_Stamp = m_Config -> m_Time . systemTime ();
+  if ( is_last )
+    this -> commit ();
   return  Http::FilterHeadersStatus::Continue;
+}
+// --------------------------------------------------------------------------
+auto  RequestCoalescingFilter::encodeData ( Buffer::Instance & data,
+                                            bool  is_last ) -> Http::FilterDataStatus
+{
+  ENVOY_LOG_MISC ( debug, "data: \"{}\", is_last: {}", data . toString (), is_last ? "true" : "false" );
+  m_Response . m_Body += data . toString ();
+  if ( is_last )
+    this -> commit ();
+  return  Http::FilterDataStatus::Continue;
+}
+// --------------------------------------------------------------------------
+auto  RequestCoalescingFilter::encodeTrailers ( Http::ResponseTrailerMap & trailers ) -> Http::FilterTrailersStatus
+{
+  m_Response . m_Trailers = Http::createHeaderMap<Http::ResponseTrailerMapImpl> ( trailers );
+  this -> commit ();
+  return  Http::FilterTrailersStatus::Continue;
 }
 // --------------------------------------------------------------------------
 // --------------------------------------------------------------------------
@@ -44,14 +124,12 @@ auto  RequestCoalescingFilterFactory::createFilterFactoryFromProtoTyped ( const 
                                                                           const std::string & ,
                                                                           Server::Configuration::FactoryContext & context ) -> Http::FilterFactoryCb
 {
-  ENVOY_LOG_MISC ( debug, "RequestCoalescingFilterFactory::createFilterFactoryFromProtoTyped ()" );
   return
   [
-    config = std::make_shared<RequestCoalescingFilterConfig> ( config, context . serverFactoryContext () ),
-    cache = std::make_shared<Cache> ()
+    config  = std::make_shared<RequestCoalescingFilterConfig> ( config, context . serverFactoryContext () ),
+    cache   = std::make_shared<Cache> ()
   ] ( Http::FilterChainFactoryCallbacks & callbacks ) -> void
   {
-    ENVOY_LOG_MISC ( debug, "RequestCoalescingFilterFactory::createFilterFactoryFromProtoTyped ()::<anonymous lambda>" );
     callbacks . addStreamFilter ( std::make_shared<RequestCoalescingFilter> ( config, cache ) );
   };
 }
--- source/extensions/filters/http/request_coalescing/request_coalescing_filter.h
+++ source/extensions/filters/http/request_coalescing/request_coalescing_filter.h
@@ -17,7 +17,9 @@ namespace  Envoy::Extensions::HttpFilters::RequestCoalescing
 
 struct  RequestCoalescingFilterConfig
 {
-  explicit  RequestCoalescingFilterConfig ( const envoy::extensions::filters::http::request_coalescing::Config & , Server::Configuration::CommonFactoryContext &  )
+  TimeSource  & m_Time;
+  explicit  RequestCoalescingFilterConfig ( const envoy::extensions::filters::http::request_coalescing::Config & , Server::Configuration::CommonFactoryContext & context )
+    : m_Time  { context . timeSource () }
   {
   }
 };
@@ -28,26 +30,46 @@ struct  Response
   std::unique_ptr<Http::ResponseTrailerMap>  m_Trailers = nullptr;
   std::string  m_Body = "";
 
-  Envoy::SystemTime  m_Stamp {};  // "response metadata"
+  Envoy::SystemTime  m_Stamp {};  // "response metadata", @note: this should really be MonotonicTime
 };
 
+/*
 using  Cache = std::unordered_map
-  < Envoy::Extensions::HttpFilters::Cache::Key  //
+  < std::string  //Envoy::Extensions::HttpFilters::Cache::Key  //
    , Response
-   , MessageUtil
-   , MessageUtil
+   //, MessageUtil
+   //, MessageUtil
    > ;
+   */
+
+struct  Cache
+{
+  mutable std::mutex  m_Mtx;
+  std::unordered_map<std::string, Response>  m_Cache;
+
+  auto  lookup ( const std::string & key ) const -> std::optional<const Response *>;
+  auto  insert ( const std::string & key,
+                 Response && value ) -> void;
+};
 
 struct  RequestCoalescingFilter : public Http::PassThroughFilter, public std::enable_shared_from_this<RequestCoalescingFilter>
 {
+  using  Self = RequestCoalescingFilter;
+
   using  Config = RequestCoalescingFilterConfig;  // associated type
-                                                  //
+
+    RequestCoalescingFilter ( std::shared_ptr<Config>  config,
+                              std::shared_ptr<Cache>  cache );
+    ~RequestCoalescingFilter ( ) override = default;
+
+
+  auto  commit ( ) -> void;
+
   std::shared_ptr<Config>  m_Config;
   std::shared_ptr<Cache>  m_Cache;
+  std::string  m_Key;
+  Response  m_Response;
 
-  explicit  RequestCoalescingFilter ( std::shared_ptr<Config>  config,
-                                      std::shared_ptr<Cache>  cache );
-    ~RequestCoalescingFilter ( ) override = default;
   // Http::StreamFilterBase
   auto  onDestroy ( ) -> void override
   {
@@ -58,12 +80,13 @@ struct  RequestCoalescingFilter : public Http::PassThroughFilter, public std::en
   // Http::StreamDecoderFilter
   auto  decodeHeaders ( Http::RequestHeaderMap & headers,
                         bool  is_last ) -> Http::FilterHeadersStatus override;
+  // i don't override decodeData() and decodeTrailers() because only the headers are needed to lookup a cache entry.
   // Http::StreamEncoderFilter
   auto  encodeHeaders ( Http::ResponseHeaderMap & headers,
                         bool  is_last ) -> Http::FilterHeadersStatus override;
-
-
-
+  auto  encodeData ( Buffer::Instance & data,
+                     bool  is_last ) -> Http::FilterDataStatus override;
+  auto  encodeTrailers ( Http::ResponseTrailerMap & trailers ) -> Http::FilterTrailersStatus override;
 };
 
 struct  RequestCoalescingFilterFactory : public Common::FactoryBase<envoy::extensions::filters::http::request_coalescing::Config>
