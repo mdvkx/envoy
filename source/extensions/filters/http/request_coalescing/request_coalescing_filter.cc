834ee398b750fc8cb6f7410ead506bbdf85ee417
Michal Dvořák <dvora124@pm.me>
2026-03-31 20:12:39 +0200

    sq

--- source/extensions/filters/http/request_coalescing/request_coalescing_filter.cc
+++ source/extensions/filters/http/request_coalescing/request_coalescing_filter.cc
@@ -16,110 +16,121 @@ namespace
 }
 
 const auto  g_CacheableStatusCodes = std::unordered_set<std::string_view>
-{  // unordered_set doesn't have a constexpr constructor, jeeeeeesus.
+{
   "200", "203", "204", "206",
   "300", "301", "308",
   "404", "405", "410", "414", "451",
   "501",
 };
-// --------------------------------------------------------------------------
+
 static auto  is_cacheable ( const Http::ResponseHeaderMap & headers ) -> bool
 {
-  if ( !g_CacheableStatusCodes . contains ( headers . getStatusValue () ) )
-    return  false;
-  //if ( auto  * p = headers . CacheControl (); p == nullptr ||   )
-  return  true;
+  return  g_CacheableStatusCodes . contains ( headers . getStatusValue () );
 }
-// --------------------------------------------------------------------------
+
 static auto  is_cacheable ( const Response & response ) -> bool
 {
-  return  response . m_Headers != nullptr && is_cacheable ( *response . m_Headers );
+  return  (
+    response . m_Headers != nullptr
+    && is_cacheable ( *response . m_Headers )
+  );
 }
+
 // --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
-auto  Cache::lookup ( const std::string & key ) const -> std::optional<const Response *>
-{
-  auto  l = std::unique_lock { m_Mtx };
-  if ( auto  i = m_Cache . find ( key ); i != m_Cache . end () )
-    return  std::addressof ( i -> second );
-  else
-    return  std::nullopt;
-}
-// --------------------------------------------------------------------------
-auto  Cache::insert ( const std::string & key,
-                      Response && value ) -> void
-{
-  auto  l = std::unique_lock { m_Mtx };
-  m_Cache [ key ] = std::move ( value );
-}
-// --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
+
       RequestCoalescingFilter::RequestCoalescingFilter ( std::shared_ptr<Config>  config,
                                                          std::shared_ptr<Cache>  cache )
-  : m_Config  { config }
-  , m_Cache  { cache }
+  : m_Config  { config },
+    m_Cache  { cache }
 {
 }
-// --------------------------------------------------------------------------
+
 auto  RequestCoalescingFilter::commit ( ) -> void
 {
   if ( !is_cacheable ( m_Response ) )
     return;
-  m_Cache -> insert ( m_Key, std::move ( m_Response ) );
-  //assert ( 0 );
+  auto  l = std::unique_lock { m_Cache -> m_Mtx };
+  m_Cache -> m_Responses . insert_or_assign ( m_Key, std::move ( m_Response ) );  // insert_or_assign because we might have to replace an expired response
+  m_Cache -> m_Pending . erase ( m_Key );
 }
-// --------------------------------------------------------------------------
+
 auto  RequestCoalescingFilter::decodeHeaders ( Http::RequestHeaderMap & headers,
-                                               bool  is_last ) -> Http::FilterHeadersStatus
+                                               bool  /* is_last - don't care because i don't need body/trailers anyway */ ) -> Http::FilterHeadersStatus
 {
   using namespace  std::literals;
   const auto  scheme = headers . getSchemeValue ();
-  const auto  host = headers . getHostValue ();
-  const auto  path = headers . getPathValue ();
-  const auto  key = absl::StrCat ( scheme, "://", host, path );
-  m_Key = key;
-  const auto  response = m_Cache -> lookup ( key );
-  if ( !response  // cache miss
-       ||  std::chrono::duration_cast<std::chrono::seconds> ( (m_Config -> m_Time . systemTime () - (*response) -> m_Stamp) ) > 60s )  // expired
-    return  Http::FilterHeadersStatus::Continue;
-  decoder_callbacks_ -> sendLocalReply ( Http::Code::OK, response . value () -> m_Body, [ response ] ( Http::ResponseHeaderMap & headers ) -> void
-  {
-    // TODO: what about original headers?
-    // TODO: response may dangle! it's a reference into a shared cache
-    headers = *response . value () -> m_Headers;
-  }, absl::nullopt, "details"sv );
-  return  Http::FilterHeadersStatus::StopAllIterationAndWatermark;
-  assert ( 0 );
+  const auto  host   = headers . getHostValue ();
+  const auto  path   = headers . getPathValue ();
+  m_Key = absl::StrCat ( scheme, "://", host, path );
+  auto  l = std::unique_lock { m_Cache -> m_Mtx };
+  if (
+    auto  i = m_Cache -> m_Responses . find ( m_Key );
+    i == m_Cache -> m_Responses . end ()
+    || std::chrono::duration_cast<std::chrono::seconds> ( m_Config -> m_Time . systemTime () - i -> second . m_Stamp ) > 60s
+  )
+  {  // cache miss or response expired
+    // is there a pending request to the same resource already sent by a different filter?
+    if ( auto  i = m_Cache -> m_Pending . find ( m_Key );  i != m_Cache -> m_Pending . end () )
+    {  // yes - subscribe to it
+      i -> second . subscribe ( this -> decoder_callbacks_ );
+      return  Http::FilterHeadersStatus::StopIteration;
+      assert ( 0 );
+    }
+    else
+    {  // no - create it
+      m_Pending = std::addressof ( m_Cache -> m_Pending . emplace_hint ( i, m_Key, Pending {} ) -> second );
+      return  Http::FilterHeadersStatus::Continue;
+    }
+  }
+  else
+  {  // cache hit
+    const auto  & response = i -> second;
+    decoder_callbacks_ -> sendLocalReply (
+      Http::Code::OK,
+      response . m_Body,
+      [ ] ( Http::ResponseHeaderMap & ) -> void { },
+      absl::nullopt,
+      ""sv
+    );
+    return  Http::FilterHeadersStatus::StopAllIterationAndWatermark;
+  }
 }
-// --------------------------------------------------------------------------
+
 auto  RequestCoalescingFilter::encodeHeaders ( Http::ResponseHeaderMap & headers,
                                                bool  is_last ) -> Http::FilterHeadersStatus
 {
   m_Response . m_Headers = Http::createHeaderMap<Http::ResponseHeaderMapImpl> ( headers );
   m_Response . m_Stamp = m_Config -> m_Time . systemTime ();
+  if ( m_Pending )
+    m_Pending -> publishHeaders ( headers, is_last );
   if ( is_last )
     this -> commit ();
   return  Http::FilterHeadersStatus::Continue;
 }
-// --------------------------------------------------------------------------
+
 auto  RequestCoalescingFilter::encodeData ( Buffer::Instance & data,
                                             bool  is_last ) -> Http::FilterDataStatus
 {
   ENVOY_LOG_MISC ( debug, "data: \"{}\", is_last: {}", data . toString (), is_last ? "true" : "false" );
   m_Response . m_Body += data . toString ();
+  if ( m_Pending )
+    m_Pending -> publishBody ( data, is_last );
   if ( is_last )
     this -> commit ();
   return  Http::FilterDataStatus::Continue;
 }
-// --------------------------------------------------------------------------
+
 auto  RequestCoalescingFilter::encodeTrailers ( Http::ResponseTrailerMap & trailers ) -> Http::FilterTrailersStatus
 {
   m_Response . m_Trailers = Http::createHeaderMap<Http::ResponseTrailerMapImpl> ( trailers );
+  if ( m_Pending )
+    m_Pending -> publishTrailers ( trailers );
   this -> commit ();
   return  Http::FilterTrailersStatus::Continue;
 }
+
 // --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
+
 auto  RequestCoalescingFilterFactory::createFilterFactoryFromProtoTyped ( const envoy::extensions::filters::http::request_coalescing::Config & config,
                                                                           const std::string & ,
                                                                           Server::Configuration::FactoryContext & context ) -> Http::FilterFactoryCb
--- source/extensions/filters/http/request_coalescing/request_coalescing_filter.h
+++ source/extensions/filters/http/request_coalescing/request_coalescing_filter.h
@@ -5,12 +5,17 @@
 #include "source/extensions/filters/http/request_coalescing/request_coalescing_filter.pb.validate.h"
 
 #include "envoy/common/time.h"
+#include "envoy/http/header_map.h"  // HeaderMap
+#include "source/common/http/header_map_impl.h"  // createHeaderMap
 #include "source/common/protobuf/utility.h"  // MessageUtil
 #include "source/extensions/filters/http/common/factory_base.h"  // FactoryBase
 #include "source/extensions/filters/http/common/pass_through_filter.h"  // PassThroughFilter
+#include <future>  // promise, shared_future
 #include <memory>  // shared, unique ptr
+#include <mutex>  // mutex
 #include <string>  // string
 #include <unordered_map>  // unordered map
+#include <variant>  // variant, monostate
 
 namespace  Envoy::Extensions::HttpFilters::RequestCoalescing
 {
@@ -33,23 +38,38 @@ struct  Response
   Envoy::SystemTime  m_Stamp {};  // "response metadata", @note: this should really be MonotonicTime
 };
 
-/*
-using  Cache = std::unordered_map
-  < std::string  //Envoy::Extensions::HttpFilters::Cache::Key  //
-   , Response
-   //, MessageUtil
-   //, MessageUtil
-   > ;
-   */
+struct  Pending
+{
+  std::vector<Http::StreamDecoderFilterCallbacks *>  m_Subscribers;
+  auto  publishHeaders ( const Http::ResponseHeaderMap & headers, bool  is_last ) -> void
+  {
+    for ( auto & p : m_Subscribers )
+      p -> encodeHeaders ( Http::createHeaderMap<Http::ResponseHeaderMapImpl> ( headers ), is_last, "dunno" );
+  }
+  auto  publishTrailers ( const Http::ResponseTrailerMap & trailers ) -> void
+  {
+    for ( auto & p : m_Subscribers )
+      p -> encodeTrailers ( Http::createHeaderMap<Http::ResponseTrailerMapImpl> ( trailers ) );
+  }
+  auto  publishBody ( const Buffer::Instance & data, bool  is_last ) -> void
+  {
+    for ( auto & p : m_Subscribers )
+    {
+      auto  copy = Buffer::OwnedImpl { data };
+      p -> encodeData ( copy, is_last );
+    }
+  }
+  auto  subscribe ( Http::StreamDecoderFilterCallbacks * p ) -> void
+  {
+    m_Subscribers . emplace_back ( p );
+  }
+};
 
 struct  Cache
 {
   mutable std::mutex  m_Mtx;
-  std::unordered_map<std::string, Response>  m_Cache;
-
-  auto  lookup ( const std::string & key ) const -> std::optional<const Response *>;
-  auto  insert ( const std::string & key,
-                 Response && value ) -> void;
+  std::unordered_map<std::string, Response>  m_Responses;
+  std::unordered_map<std::string, Pending>  m_Pending;
 };
 
 struct  RequestCoalescingFilter : public Http::PassThroughFilter, public std::enable_shared_from_this<RequestCoalescingFilter>
@@ -69,6 +89,7 @@ struct  RequestCoalescingFilter : public Http::PassThroughFilter, public std::en
   std::shared_ptr<Cache>  m_Cache;
   std::string  m_Key;
   Response  m_Response;
+  Pending * m_Pending = nullptr;
 
   // Http::StreamFilterBase
   auto  onDestroy ( ) -> void override
@@ -80,7 +101,7 @@ struct  RequestCoalescingFilter : public Http::PassThroughFilter, public std::en
   // Http::StreamDecoderFilter
   auto  decodeHeaders ( Http::RequestHeaderMap & headers,
                         bool  is_last ) -> Http::FilterHeadersStatus override;
-  // i don't override decodeData() and decodeTrailers() because only the headers are needed to lookup a cache entry.
+    // i don't override decodeData() and decodeTrailers() because the headers are all that's needed to lookup a cache entry.
   // Http::StreamEncoderFilter
   auto  encodeHeaders ( Http::ResponseHeaderMap & headers,
                         bool  is_last ) -> Http::FilterHeadersStatus override;
