23455a495e9a62befe656218da3f0a68f9b9f6f3
Michal Dvořák <dvora124@pm.me>
2026-04-09 11:25:00 +0200

    sq

---
+++ source/extensions/filters/http/cache2/cache.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "./response.h"
+
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+struct  Cache
+{
+  std::mutex  m_Mtx;
+  std::unordered_map<std::string, std::shared_ptr<const Response> >  m_Responses;
+  [[nodiscard]]
+  auto  lookup ( const std::string & k ) const -> std::optional<std::shared_ptr<const Response> >
+  {
+    auto  l = std::unique_lock { m_Mtx };
+    if ( const auto  i = m_Responses . find ( k ); i == m_Responses . end () )
+      return  std::nullopt;
+    else
+      return  i -> second;
+  }
+  auto  insert ( const std::string & k, std::shared_ptr<const Response>  v ) -> void
+  {
+    auto  l = std::unique_lock { m_Mtx };
+    m_Responses . insert_or_assign ( k, std::move ( v ) );
+  }
+};
