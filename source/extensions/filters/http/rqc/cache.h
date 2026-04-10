c7e1b96dfa1ad0ea28be02baedf75ab29fe4bcc3
Michal Dvořák <dvora124@pm.me>
2026-04-10 13:08:10 +0200

    sq

---
+++ source/extensions/filters/http/rqc/cache.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+namespace  Envoy::Extensions::HttpFilters::Rqc {
+
+
+struct  Cache
+{
+  mutable std::mutex  m_Mtx;
+  std::unordered_map<std::string, std::size_t>  m_Elements;
+  auto  insert_or ( const std::string & k,
+                    const std::function<std::size_t ()> & lazy,
+                    const std::function<void (std::size_t &)> & modify ) -> bool
+  {
+    auto  l = std::unique_lock { m_Mtx };
+    auto  i = m_Elements . find ( k );
+    if ( i == m_Elements . end () )
+    {
+      m_Elements . emplace_hint ( i, k, lazy () );
+      return  true;
+    }
+    else
+    {
+      modify ( i -> second );
+      return  false;
+    }
+  }
+};
+
+}
