cda7dea0db09f4d3b9d59de8be2ab370f6c9bbbc
Michal Dvořák <dvora124@pm.me>
2026-04-13 15:32:21 +0200

    sq

--- source/extensions/filters/http/cache2/filter.h
+++ source/extensions/filters/http/cache2/filter.h
@@ -33,7 +33,7 @@ enum struct  State
   Destroyed,
 };
 
-struct  Filter : public Http::PassThroughFilter, public Logger::Loggable<Logger::Id::cache_filter>, public std::enable_shared_from_this<Filter>
+struct  Filter : public Http::PassThroughFilter, public Logger::Loggable<Logger::Id::filter>, public std::enable_shared_from_this<Filter>
 {
   State  m_State = State::Unknown;
 
