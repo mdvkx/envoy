d6d23c930474ff146e685161078d6e1dfc399191
Michal Dvořák <dvora124@pm.me>
2026-04-14 16:55:18 +0200

    sq

--- source/extensions/filters/http/rqc/filter.cc
+++ source/extensions/filters/http/rqc/filter.cc
@@ -26,10 +26,8 @@ auto  Filter::decodeHeaders  ( Http::RequestHeaderMap & headers,
   std::size_t  cnt = 0;
   if ( m_Cache -> insert_or ( m_Key, [ ] ( ) { return  Pending {}; }, [ this, &cnt ] ( Pending & p ) -> void
   {
-    p . m_Waiting . emplace_back ( [ wp = this -> weak_from_this () ] ( Msg && msg ) mutable -> void
+    p . m_Waiting . emplace_back ( [ p = this -> shared_from_this () ] ( Msg && msg ) mutable -> void
     {
-      auto  p = wp . lock ();
-      assert ( p );
       p -> msg ( std::move ( msg ) );
     } );
     cnt = p . m_Waiting . size ();  // dirty hack, temporary
