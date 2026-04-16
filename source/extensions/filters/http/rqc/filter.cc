ba59a7cb6b51fdd269b8f3fbb0c4402b9b63bf2b
Michal Dvořák <dvora124@pm.me>
2026-04-16 15:15:21 +0200

    sq

--- source/extensions/filters/http/rqc/filter.cc
+++ source/extensions/filters/http/rqc/filter.cc
@@ -11,33 +11,57 @@ namespace  Envoy::Extensions::HttpFilters::Rqc
 
 auto  Filter::onDestroy ( ) -> void
 {
-  ENVOY_LOG ( debug, "@@@ destroy" );
+  ENVOY_LOG (
+    debug,
+    "@@@ [stream id = {:08x}] destroy",
+    this -> decoder_callbacks_ -> streamId ()
+  );
 }
 
 auto  Filter::decodeHeaders ( Http::RequestHeaderMap & headers,
                               bool  is_last ) -> Http::FilterHeadersStatus
 {
-  ENVOY_LOG ( debug, "@@@ decoding headers // stream id = {:08x}", this -> decoder_callbacks_ -> streamId () );
+  ENVOY_LOG (
+    debug,
+    "@@@ [stream id = {:08x}] decoding headers",
+    this -> decoder_callbacks_ -> streamId ()
+  );
+
   return  Http::FilterHeadersStatus::Continue;
 }
 
 auto  Filter::encodeHeaders ( Http::ResponseHeaderMap & headers,
                               bool  is_last ) -> Http::FilterHeadersStatus
 {
-  ENVOY_LOG ( debug, "@@@ encoding headers" );
+  ENVOY_LOG (
+    debug,
+    "@@@ [stream id = {:08x}] encoding headers",
+    this -> decoder_callbacks_ -> streamId ()
+  );
+
   return  Http::FilterHeadersStatus::Continue;
 }
 
 auto  Filter::encodeTrailers ( Http::ResponseTrailerMap & trailers ) -> Http::FilterTrailersStatus
 {
-  ENVOY_LOG ( debug, "@@@ encoding trailers" );
+  ENVOY_LOG (
+    debug,
+    "@@@ [stream id = {:08x}] encoding trailers",
+    this -> decoder_callbacks_ -> streamId ()
+  );
+
   return  Http::FilterTrailersStatus::Continue;
 }
 
 auto  Filter::encodeData    ( Buffer::Instance & body,
                               bool  is_last ) -> Http::FilterDataStatus
 {
-  ENVOY_LOG ( debug, "@@@ encoding {} bytes body", body . length () );
+  ENVOY_LOG (
+    debug,
+    "@@@ [stream id = {:08x}] encoding {} bytes body",
+    this -> decoder_callbacks_ -> streamId (), body . length ()
+  );
+
   return  Http::FilterDataStatus::Continue;
 }
 
