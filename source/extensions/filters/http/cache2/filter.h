7f4926c76639f7160a3cbb6de2521754781fa33c
Michal Dvořák <dvora124@pm.me>
2026-04-17 14:37:33 +0200

    sq

--- source/extensions/filters/http/cache2/filter.h
+++ source/extensions/filters/http/cache2/filter.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "./ring.h"
+
 #include "envoy/buffer/buffer.h"  // Buffer::Instance
 #include "envoy/common/time.h"  // SystemTime
 #include "envoy/http/header_map.h"  // RequestHeaderMap
